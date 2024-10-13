d31710cf4d73e39533678d91062379a9c2d8c8e2
Mechantchulo <chulomechant@gmail.com>
2024-10-13 23:34:55 +0300

    initial commit

---
+++ reference.java
@@ -0,0 +1,19 @@
+//Objects and classes
+class  student{
+    String name;
+    int id;
+    String regno;
+}
+ public class reference{
+    public static void main(String[] args){
+        student s1 = new student();
+        s1.name = "Erick";
+        s1.id = 15517;
+        s1.regno = "S13/02989/23";
+
+        System.out.println("Name: "+s1.name);
+        System.out.println("Id: "+s1.id);
+        System.out.println("Regno: "+s1.regno);
+
+ }
+}
